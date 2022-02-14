ecb362759d8f4039011adf37cc97391b577bbacb
Ilia <Galilia@users.noreply.github.com>
2022-02-14 20:19:37 +0200

    fortinite shop with usestate and useeffect

---
+++ src/components/Shop.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from "react";
+import {Movies} from '../components/Movies';
+import {Preloader} from "../components/Preloader";
+import Search from "../components/Search";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function Main() {
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+      .then(repsonse => repsonse.json())
+      .then(data => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      })
+  }, []);
+
+  const searchMovies = (str, type = 'all') => {
+    setLoading(true);
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+      .then(repsonse => repsonse.json())
+      .then(data => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      })
+  }
+
+  return <main className="container content">
+    <Search searchMovies={searchMovies}/>
+    { loading ? <Preloader /> : <Movies movies={movies}/> }
+  </main>
+}
+
+export { Main }
